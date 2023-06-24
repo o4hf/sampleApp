c5dcc37602ddca91954629bebb71e8f0fc4b3298
Anand Pandiyan <apandiyan@gmail.com>
2023-06-24 14:48:36 +0400

    Initial Commit

---
+++ sampleApp/sampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleAppApp.swift
+//  sampleApp
+//
+//  Created by apandiyan on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct sampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
